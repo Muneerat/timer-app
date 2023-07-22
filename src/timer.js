aeffc078ed90b351e35748d50901995cd2d826c0
Muneerat <oladipomunirat@gmail.com>
2023-07-22 17:32:00 +0100

    Implement timer app

--- src/timer.js
+++ src/timer.js
@@ -10,17 +10,21 @@ const Timer = () => {
   useEffect(() => {
     let timeInterval = null;
 
+    // If the timer is Counting, set up an interval to increment the seconds
+
     if (isCounting) {
       timeInterval = setInterval(() => {
         setSeconds((prev) => prev + 1);
       }, 1000);
     }
 
+    // Clean up the interval
     return () => {
       clearInterval(timeInterval);
     };
   }, [isCounting]);
 
+  // Function to format the time in minutes and seconds
   const timeFormat = (time) => {
     let minutes = Math.floor(time / 60);
     let seconds = time % 60;
@@ -31,16 +35,16 @@ const Timer = () => {
     return `${minFormatted}:${secFormatted}`;
   };
 
+  // Function to handle the Play/Pause button click
   const handlePlayPaues = () => {
     setIsCounting((play) => !play);
   };
 
+  // Function to handle the Stop button click
   const handleStop = () => {
     setIsCounting(false);
     setSeconds(0);
   };
-
-  // console.log(timeInterval);
 
   return (
     <div className="flex justify-center p-10 border-2 mx-auto m-10 md:w-8/12 w-12/12 rounded-3xl h-full">
